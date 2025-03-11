336869d110727a385d58aa3dc1f735e0b6c1c9c0
Chandsurendra <101378768+Chandsurendra@users.noreply.github.com>
2025-03-11 22:39:34 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,9 @@ const gitInfo = getGitInfo();
 
 export default defineConfig((config) => {
   return {
+    server: {
+    allowedHosts: ['bolt-diy-81fu.onrender.com'] // Add your Render subdomain here
+  },
     define: {
       __COMMIT_HASH: JSON.stringify(gitInfo.commitHash),
       __GIT_BRANCH: JSON.stringify(gitInfo.branch),
